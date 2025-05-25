e9ea107b0ad9fefde705f4d58ecd5625d49b64e2
leeonscoding <leeonscoding@gmail.com>
2025-05-25 15:35:11 +0600

    refactoring

--- src/main/java/com/leeonscoding/personal_apps/repositories/LoanRepository.java
+++ src/main/java/com/leeonscoding/personal_apps/repositories/LoanRepository.java
@@ -3,7 +3,5 @@ package com.leeonscoding.personal_apps.repositories;
 import com.leeonscoding.personal_apps.entities.loan.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
