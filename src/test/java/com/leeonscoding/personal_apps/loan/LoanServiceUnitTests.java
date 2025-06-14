0bde11b774b1f42887a3cf8db836a1b3de041eff
leeonscoding <leeonscoding@gmail.com>
2025-06-14 10:10:29 +0600

    service unit tests

--- src/test/java/com/leeonscoding/personal_apps/loan/LoanServiceUnitTests.java
+++ src/test/java/com/leeonscoding/personal_apps/loan/LoanServiceUnitTests.java
@@ -1,19 +1,27 @@
 package com.leeonscoding.personal_apps.loan;
 
+import com.leeonscoding.personal_apps.dtos.SortOrders;
 import com.leeonscoding.personal_apps.dtos.loan.LoanDTO;
+import com.leeonscoding.personal_apps.dtos.loan.LoanSearchCriteria;
+import com.leeonscoding.personal_apps.dtos.loan.LoanSortFields;
 import com.leeonscoding.personal_apps.entities.loan.CategoryType;
 import com.leeonscoding.personal_apps.entities.loan.StatusType;
 import com.leeonscoding.personal_apps.services.loan.LoanServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LoanServiceUnitTests {
 
     @Autowired
@@ -21,7 +29,7 @@ public class LoanServiceUnitTests {
 
     LoanDTO newLoan = null;
 
-    @BeforeEach
+    @BeforeAll
     void setup() {
         LoanDTO loanDTO = new LoanDTO(0, "kosai", 7000, null, CategoryType.TAKE, StatusType.UNPAID);
 
@@ -51,9 +59,43 @@ public class LoanServiceUnitTests {
         loanService.createLoan(loanDTO2);
         loanService.createLoan(loanDTO3);
 
-        List<LoanDTO> loans = loanService.getLoans("bou");
+        LocalDate nowDate = LocalDate.now();
+        int month = nowDate.getMonth().getValue();
+        int year = nowDate.getYear();
+
+        LoanSearchCriteria searchCriteria = new LoanSearchCriteria(month, year, Optional.of("mama"), Optional.empty(), Optional.empty());
+
+        List<LoanDTO> loans = loanService.getLoans(searchCriteria, LoanSortFields.NAME, SortOrders.DESC);
 
         assertEquals(1, loans.size());
+    }
+
+    @Test
+    void editTest() {
+        LoanDTO updateDto = new LoanDTO(newLoan.id(), "kosai", 7000, null, CategoryType.GIVE, StatusType.UNPAID);
+        loanService.updateLoan(newLoan.id(), updateDto);
+
+        LoanDTO loan = loanService.getLoanById(newLoan.id());
+
+        assertEquals(newLoan.amount(), loan.amount());
+    }
+
+    @Test
+    void deleteTest() {
+
+        String name = newLoan.name();
+
+        loanService.deleteLoan(newLoan.id());
+
+        LocalDate nowDate = LocalDate.now();
+        int month = nowDate.getMonth().getValue();
+        int year = nowDate.getYear();
+
+        LoanSearchCriteria searchCriteria = new LoanSearchCriteria(month, year, Optional.of(name), Optional.empty(), Optional.empty());
+
+        List<LoanDTO> loans = loanService.getLoans(searchCriteria, LoanSortFields.NAME, SortOrders.DESC);
+
+        assertEquals(0, loans.size());
     }
 
 }
